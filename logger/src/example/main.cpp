053c0ef1bcf72cb8c8c0ef7be5d226c6a6780042
Artem <mailforallaccs@gmail.com>
2026-01-21 18:33:04 +0900

    Setting up a project

---
+++ logger/src/example/main.cpp
@@ -0,0 +1,11 @@
+#include <print>
+#include <iostream>
+#include "logger/logger.h"
+
+int main() {
+
+	std::println("Hello, World!");
+	std::cin.get();
+
+	return 0;
+}
---
+++ logger/src/logger/logger.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class ConsoleLogger {
+
+public:
+
+private:
+
+};
+
+ConsoleLogger Log;
