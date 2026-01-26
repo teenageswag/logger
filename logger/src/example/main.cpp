649b9f2c48eff4431d7f4bf46b185d107259dd18
Artem <mailforallaccs@gmail.com>
2026-01-26 20:00:05 +0900

    Now the console and file logger can be enabled as desired.

--- logger/src/example/main.cpp
+++ logger/src/example/main.cpp
@@ -1,42 +1,45 @@
-#include <print>
 #include <iostream>
+#include <print>
+
+#define CONSOLE_LOGGER_ENABLE
+#define FILE_LOGGER_ENABLE
 #include "logger/logger.h"
 
 int main() {
-	// Console Logger Preview
-	g_ConsoleLogger.Info("Output to the console");
-	g_ConsoleLogger.Info("{} output to the console", "Formatted");
-	g_ConsoleLogger.Separator();
+  // Console Logger Preview
+  g_ConsoleLogger.Info("Output to the console");
+  g_ConsoleLogger.Info("{} output to the console", "Formatted");
+  g_ConsoleLogger.Separator();
 
-	g_ConsoleLogger.Warn("Output to the console");
-	g_ConsoleLogger.Warn("{} output to the console", "Formatted");
-	g_ConsoleLogger.Separator();
+  g_ConsoleLogger.Warn("Output to the console");
+  g_ConsoleLogger.Warn("{} output to the console", "Formatted");
+  g_ConsoleLogger.Separator();
 
-	g_ConsoleLogger.Error("Output to the console");
-	g_ConsoleLogger.Error("{} output to the console", "Formatted");
-	g_ConsoleLogger.Separator();
+  g_ConsoleLogger.Error("Output to the console");
+  g_ConsoleLogger.Error("{} output to the console", "Formatted");
+  g_ConsoleLogger.Separator();
 
-	g_ConsoleLogger.Success("Output to the console");
-	g_ConsoleLogger.Success("{} output to the console", "Formatted");
-	g_ConsoleLogger.Separator();
+  g_ConsoleLogger.Success("Output to the console");
+  g_ConsoleLogger.Success("{} output to the console", "Formatted");
+  g_ConsoleLogger.Separator();
 
-	// File Logger Preview
-	g_FileLogger.Info("Output to the log.txt");
-	g_FileLogger.Info("{} output to the log.txt", "Formatted");
-	g_FileLogger.Separator();
+  // File Logger Preview
+  g_FileLogger.Info("Output to the log.txt");
+  g_FileLogger.Info("{} output to the log.txt", "Formatted");
+  g_FileLogger.Separator();
 
-	g_FileLogger.Warn("Output to the log.txt");
-	g_FileLogger.Warn("{} output to the log.txt", "Formatted");
-	g_FileLogger.Separator();
+  g_FileLogger.Warn("Output to the log.txt");
+  g_FileLogger.Warn("{} output to the log.txt", "Formatted");
+  g_FileLogger.Separator();
 
-	g_FileLogger.Error("Output to the log.txt");
-	g_FileLogger.Error("{} output to the log.txt", "Formatted");
-	g_FileLogger.Separator();
+  g_FileLogger.Error("Output to the log.txt");
+  g_FileLogger.Error("{} output to the log.txt", "Formatted");
+  g_FileLogger.Separator();
 
-	g_FileLogger.Success("Output to the log.txt");
-	g_FileLogger.Success("{} output to the log.txt", "Formatted");
-	g_FileLogger.Separator();
+  g_FileLogger.Success("Output to the log.txt");
+  g_FileLogger.Success("{} output to the log.txt", "Formatted");
+  g_FileLogger.Separator();
 
-	std::cin.get();
-	return 0;
+  std::cin.get();
+  return 0;
 }
--- logger/src/logger/logger.h
+++ logger/src/logger/logger.h
@@ -2,14 +2,14 @@
 #include <chrono>
 #include <concepts>
 #include <cstdint>
+#include <filesystem>
 #include <format>
+#include <fstream>
+#include <mutex>
 #include <print>
 #include <string>
 #include <string_view>
 #include <windows.h>
-#include <mutex>
-#include <filesystem>
-#include <fstream>
 
 //  ======Global======
 enum class LogLevel : uint8_t { Info, Warn, Error, Success };
@@ -22,11 +22,16 @@ struct LogUtils {
   }
   static constexpr std::string_view GetLevelPrefix(LogLevel level) noexcept {
     switch (level) {
-    case LogLevel::Info:    return "INF";
-    case LogLevel::Warn:    return "WRN";
-    case LogLevel::Error:   return "ERR";
-    case LogLevel::Success: return "SUC";
-    default:                return "UNK";
+    case LogLevel::Info:
+      return "INF";
+    case LogLevel::Warn:
+      return "WRN";
+    case LogLevel::Error:
+      return "ERR";
+    case LogLevel::Success:
+      return "SUC";
+    default:
+      return "UNK";
     }
   }
 };
@@ -34,7 +39,8 @@ struct LogUtils {
 //  ======Console Logger======
 struct Color {
   uint8_t r, g, b;
-  constexpr Color(uint8_t r = 255, uint8_t g = 255, uint8_t b = 255) noexcept : r(r), g(g), b(b) {}
+  constexpr Color(uint8_t r = 255, uint8_t g = 255, uint8_t b = 255) noexcept
+      : r(r), g(g), b(b) {}
 
   //  Convert HEX to RGB
   static constexpr Color FromHex(uint32_t hex) noexcept {
@@ -42,7 +48,6 @@ struct Color {
                  static_cast<uint8_t>((hex >> 8) & 0xFF),
                  static_cast<uint8_t>(hex & 0xFF));
   }
-
   //  Get ANSI escape sequence for this color
   [[nodiscard]] auto ToAnsi() const noexcept {
     return std::format("\x1b[38;2;{};{};{}m", r, g, b);
@@ -50,16 +55,16 @@ struct Color {
 };
 
 namespace LogColors {
-    inline constexpr Color Timestamp = Color::FromHex(0x8C8C8C);
-    inline constexpr Color Info = Color::FromHex(0x1A8CFF);
-    inline constexpr Color Warn = Color::FromHex(0xFF8C1A);
-    inline constexpr Color Error = Color::FromHex(0xFF1A40);
-    inline constexpr Color Success = Color::FromHex(0x1AFF1A);
-    inline constexpr Color Default = Color::FromHex(0xE6E6E6);
-    inline constexpr Color Separator = Color::FromHex(0x8C8C8C);
-}
+inline constexpr Color Timestamp = Color::FromHex(0x8C8C8C);
+inline constexpr Color Info = Color::FromHex(0x1A8CFF);
+inline constexpr Color Warn = Color::FromHex(0xFF8C1A);
+inline constexpr Color Error = Color::FromHex(0xFF1A40);
+inline constexpr Color Success = Color::FromHex(0x1AFF1A);
+inline constexpr Color Default = Color::FromHex(0xE6E6E6);
+inline constexpr Color Separator = Color::FromHex(0x8C8C8C);
+} // namespace LogColors
 
-
+#ifdef CONSOLE_LOGGER_ENABLE
 class ConsoleLogger {
 private:
   static inline HANDLE s_HandleConsole = nullptr;
@@ -68,11 +73,16 @@ private:
 
   static constexpr Color GetLevelColor(LogLevel level) noexcept {
     switch (level) {
-        case LogLevel::Info: return LogColors::Info;
-        case LogLevel::Warn: return LogColors::Warn;
-        case LogLevel::Error: return LogColors::Error;
-        case LogLevel::Success: return LogColors::Success;
-        default: return LogColors::Default;
+    case LogLevel::Info:
+      return LogColors::Info;
+    case LogLevel::Warn:
+      return LogColors::Warn;
+    case LogLevel::Error:
+      return LogColors::Error;
+    case LogLevel::Success:
+      return LogColors::Success;
+    default:
+      return LogColors::Default;
     }
   }
   static int GetConsoleWidth() noexcept {
@@ -89,23 +99,18 @@ private:
     if (!s_initialized) [[unlikely]]
       Initialize();
 
-    std::print(
-        "{}[{}] {}[{}]{} {}\n",
-        LogColors::Timestamp.ToAnsi(),
-        LogUtils::GetTimestamp(), GetLevelColor(level).ToAnsi(),
-        LogUtils::GetLevelPrefix(level), s_ResetSeq, message
-    );
+    std::print("{}[{}] {}[{}]{} {}\n", LogColors::Timestamp.ToAnsi(),
+               LogUtils::GetTimestamp(), GetLevelColor(level).ToAnsi(),
+               LogUtils::GetLevelPrefix(level), s_ResetSeq, message);
   }
 
   static void Initialize() noexcept {
     if (s_initialized) [[likely]]
       return;
 
-    s_HandleConsole = CreateFileA(
-        "CONOUT$", GENERIC_READ | GENERIC_WRITE,
-        FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
-        OPEN_EXISTING, 0, NULL
-    );
+    s_HandleConsole = CreateFileA("CONOUT$", GENERIC_READ | GENERIC_WRITE,
+                                  FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+                                  OPEN_EXISTING, 0, NULL);
 
     SetConsoleTitleA("[CONSOLE DEBUG LOGGER] | Created by j2cks");
     SetConsoleOutputCP(CP_UTF8);
@@ -125,45 +130,41 @@ private:
   }
 
   static void CreateConsole() {
-      if (GetConsoleWindow() != NULL) {
-          Initialize();
-      }
-      else {
-          AllocConsole();
-          Initialize();
-      }
+    if (GetConsoleWindow() != NULL) {
+      Initialize();
+    } else {
+      AllocConsole();
+      Initialize();
+    }
   }
   static void DestroyConsole() noexcept {
-      if (s_initialized) {
-          if (stdout) fclose(stdout);
-          if (stderr) fclose(stderr);
-          if (stdin) fclose(stdin);
+    if (s_initialized) {
+      if (stdout)
+        fclose(stdout);
+      if (stderr)
+        fclose(stderr);
+      if (stdin)
+        fclose(stdin);
 
-          if (s_HandleConsole && s_HandleConsole != INVALID_HANDLE_VALUE) {
-              CloseHandle(s_HandleConsole);
-              s_HandleConsole = nullptr;
-          }
-
-          FreeConsole();
-          s_initialized = false;
+      if (s_HandleConsole && s_HandleConsole != INVALID_HANDLE_VALUE) {
+        CloseHandle(s_HandleConsole);
+        s_HandleConsole = nullptr;
       }
+
+      FreeConsole();
+      s_initialized = false;
+    }
   }
 
 public:
-    ConsoleLogger() {
-        CreateConsole();
-    }
-    ~ConsoleLogger() {
-        DestroyConsole();
-    }
+  ConsoleLogger() { CreateConsole(); }
+  ~ConsoleLogger() { DestroyConsole(); }
 
-    //  ======Basic Console Logger======
+  //  ======Basic Console Logger======
   static void Info(std::string_view msg) { LogImpl(LogLevel::Info, msg); }
   static void Warn(std::string_view msg) { LogImpl(LogLevel::Warn, msg); }
   static void Error(std::string_view msg) { LogImpl(LogLevel::Error, msg); }
-  static void Success(std::string_view msg) {
-    LogImpl(LogLevel::Success, msg);
-  }
+  static void Success(std::string_view msg) { LogImpl(LogLevel::Success, msg); }
 
   //  ======Formatted Console Logger======
   template <typename... Args>
@@ -217,8 +218,11 @@ public:
   }
   static void NewLine() noexcept { std::println(""); }
 };
+inline ConsoleLogger g_ConsoleLogger;
+#endif // #define CONSOLE_LOGGER_ENABLE
 
 //  ======File Logger======
+#ifdef FILE_LOGGER_ENABLE
 class FileLogger {
 private:
   static inline std::ofstream s_LogFile;
@@ -252,11 +256,8 @@ private:
     }
     std::lock_guard lock(s_Mutex);
 
-    s_LogFile << std::format("[{}] [{}] {}\n",
-        LogUtils::GetTimestamp(),
-        LogUtils::GetLevelPrefix(level),
-        message
-    );
+    s_LogFile << std::format("[{}] [{}] {}\n", LogUtils::GetTimestamp(),
+                             LogUtils::GetLevelPrefix(level), message);
 
     s_LogFile.flush();
   }
@@ -264,7 +265,8 @@ private:
 public:
   FileLogger() { Initialize(); }
   ~FileLogger() {
-      if (s_LogFile.is_open()) s_LogFile.close();
+    if (s_LogFile.is_open())
+      s_LogFile.close();
   }
 
   //  ======Basic File Logger======
@@ -292,12 +294,13 @@ public:
   }
 
   //  ======File Utilities======
-  static void Separator() noexcept{
+  static void Separator() noexcept {
     if (!s_initialized) [[unlikely]]
       Initialize();
 
     if (s_LogFile.is_open()) {
-      s_LogFile << "--------------------------------------------------------------------------------\n";
+      s_LogFile << "-----------------------------------------------------------"
+                   "---------------------\n";
       s_LogFile.flush();
     }
   }
@@ -308,6 +311,5 @@ public:
     }
   }
 };
-
-inline ConsoleLogger g_ConsoleLogger;
 inline FileLogger g_FileLogger;
+#endif // #define FILE_LOGGER_ENABLE
